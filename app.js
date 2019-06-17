84918db3cc32360516766f932db5b48d75cd4dd4
Apostolis Gerodimos <gerodiak@auth.gr>
2019-06-17 20:12:10 +0300

    added Registger Routes

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const app = express();
-const mongodb = require('mongodb');
+const mongodb = require('mongodb').MongoClient;
 require('dotenv').config();
-const server= require('http').createServer(app);
-app.set('view engine' , 'pug');
+const server = require('http').createServer(app);
+const registerRoutes = require('./routes/register-routes');
+const loginRoutes = require('./routes/login-routes');
+const bodyParser = require('body-parser');
 
-mongodb.connect(process.env.MONGO_URI ,{useNewUrlParser: true}, () => {
-    console.log('Mongo db CONNECTED...');
+
+mongodb.connect(process.env.MONGO_URI ,{useNewUrlParser: true}, (err,db) => {
+    // console.log('Mongo db CONNECTED...');
+    
+    //Use body-parser so we can get body arguments on the request
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }))
 
     //set up static files under public folder
     app.use('/public', express.static(__dirname + '/public'));
     
+    //set view engine 
+    app.set('view engine' , 'pug');
+
+
     // Create home Route
     app.get('/',(req,res) => {
         res.render('home')
     });
 
+    //Set Routes
+    app.use('/register' , registerRoutes);
+    app.use('/login', loginRoutes);
+
+    
     server.listen(process.env.PORT || 4000,'192.168.1.8', () => {
         console.log( `app is now listening to port ${process.env.PORT || 4000}`);
     })
---
+++ routes/register-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const mongodb = require('mongodb').MongoClient; 
+require('dotenv').config();
+
+router.get('/' , (req,res) => {
+    res.render('register');
+} )
+
+router.post('/auth', (req,res) => {
+
+    mongodb.connect(process.env.MONGO_URI ,{useNewUrlParser: true}, (err,db) => {
+        console.log('Mongo db CONNECTED...');
+        let username = req.body.username, testDB = db.db('test'), users = testDB.collection('users');
+    
+        users.findOne({username:username},(err,db_res) => {
+            if(db_res == null){
+
+                users.insertOne({username: username, password: req.body.password},(ins_err,ins_res) => {
+                    if(ins_err) throw err;
+                    else {
+                        console.log('1 Doc inserted' + ` ${ins_res}`);
+                        db.close();
+                        res.render('home' , {registerSuccess: true});
+                    }
+                })
+            }  
+            else {
+                
+                db.close();
+                res.render('home' ,{alreadyExists: true} )
+            }
+        })
+    })
+
+})
+
+
+module.exports = router;
